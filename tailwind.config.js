9c336da846e4decc5a71aeea22f155c70d68dbbd
Santama <santamajati@gmail.com>
2024-08-15 15:13:33 +0700

    edit tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"
-  ],
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"
   ],
   theme: {
     extend: {
