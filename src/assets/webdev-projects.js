f6d975fb5217e1bce80359f1ab36789dfcd9f786
Santama <santamajati@gmail.com>
2024-08-15 19:23:38 +0700

    update project data

--- src/assets/webdev-projects.js
+++ src/assets/webdev-projects.js
@@ -8,7 +8,7 @@ const webdevProjects = [
         "name": "Maknews",
         "desc": "Maknews is a project developed using ReactJS that aims to deliver the latest news quickly and accurately. By leveraging the services of currentsapi.services, Maknews can display up-to-date news from various reliable sources. Users can easily access the latest information presented in a responsive and intuitive interface, ensuring a pleasant and efficient news-reading experience.",
         "img": maknewsImg,
-        "url": "https://maknews-bysantamaa.vercel.app",
+        "url": "https://santamaa.github.io/maknews",
         "tech": ["React", "Tailwind CSS", "Currents API"]
     },
     {
@@ -17,7 +17,7 @@ const webdevProjects = [
         "name": "Tokotok",
         "desc": "The TokoTok landing page project involves creating an engaging and modern entry point for the brand using ReactJS and TailwindCSS. The landing page will feature a captivating hero section, a product showcase with data from dummyjson.com. By integrating dynamic ReactJS components and leveraging TailwindCSS for a responsive design, the page will provide a visually appealing and functional experience that highlights TokoTok's key offerings and enhances user engagement.",
         "img": tokotokImg,
-        "url": "https://tokotok-bysantamaa.vercel.app",
+        "url": "https://santamaa.github.io/tokotok",
         "tech": ["React", "Tailwind CSS", "DummyJSON"]
     }
 ]
