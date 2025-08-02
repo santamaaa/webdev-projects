165285ae4503b52c5a0d6da69ea7fd7adc1c5e7a
santama <santamajati@gmail.com>
2025-08-02 16:56:55 +0700

    change author name

--- src/App.js
+++ src/App.js
@@ -30,15 +30,15 @@ function App() {
     <div className="w-full min-h-screen flex justify-center bg-myblack3">
         <div className="w-full max-w-[1440px] px-6 md:px-20 lg:px-40">
             <div className="mt-4 md:mt-12 flex items-center justify-between">
-                <h2 className="text-mywhite2 text-xl md:text-2xl font-bold">Santamaa</h2>
-                <a className="pl-8 py-2 flex items-center text-right" href="https://santamaa.github.io/profile-page">
+                <h2 className="text-mywhite2 text-xl md:text-2xl font-bold">Santamaaa</h2>
+                <a className="pl-8 py-2 flex items-center text-right" href="https://santamaaa.github.io/profile-page">
                     <FontAwesomeIcon className="text-mywhite2 text-2xl ease-in duration-300 hover:scale-125" icon={ faArrowRight } />
                 </a>
             </div>
             <div className="my-20 md:my-36 lg:my-28 text-mywhite2 text-right">
                 <h1 className="text-4xl md:text-6xl font-extrabold">I am</h1>
                 <h1 className="mt-2 mb-6 md:mb-12 text-4xl md:text-6xl font-extrabold">Web Developer</h1>
-                <p className="text-xs md:text-sm lg:tracking-wide">I am a web development enthusiast who is always eager to explore the latest technologies and trends in the web development world. Although I am not yet a professional web developer, I am actively learning various programming such as HTML, CSS, JavaScript, Python, SQL, and NoSQL, as well as popular frameworks and libraries such as Bootstrap, Tailwind CSS, jQuery, React, and Flask. I often spend my free time working on personal projects and taking online courses. With a deep interest in both front-end and back-end development, I am committed to continually learning and growing in this field, with the hope of one day contributing professionally to the web development industry.</p>
+                <p className="text-xs md:text-sm/6  lg:tracking-wide">I'm a web development enthusiast actively learning both front-end and back-end technologies. I'm building skills in HTML, CSS, JavaScript, Python, SQL, NoSQL, and tools like React, Vue.js, Nuxt, Flask, and FastAPI. I spend my free time on personal projects and online courses, driven by a strong passion to grow and eventually contribute to the web development industry.</p>
                 <div className="mt-12 md:mt-20 flex justify-end gap-4 md:gap-8">
                     <a className="w-10 md:w-12 h-10 md:h-12 grid place-items-center border-2 border-mywhite2 rounded-full bg-mywhite2 text-myblack3 text-xl md:text-2xl ease-in duration-300 hover:bg-myblack3 hover:text-mywhite2 hover:scale-125" href="https://instagram.com/santamaaa" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={ faInstagram } />
@@ -46,7 +46,7 @@ function App() {
                     <a className="w-10 md:w-12 h-10 md:h-12 grid place-items-center border-2 border-mywhite2 rounded-full bg-mywhite2 text-myblack3 text-xl md:text-2xl ease-in duration-300 hover:bg-myblack3 hover:text-mywhite2 hover:scale-125" href="https://linkedin.com/in/santamajati" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={ faLinkedinIn } />
                     </a>
-                    <a className="w-10 md:w-12 h-10 md:h-12 grid place-items-center border-2 border-mywhite2 rounded-full bg-mywhite2 text-myblack3 text-xl md:text-2xl ease-in duration-300 hover:bg-myblack3 hover:text-mywhite2 hover:scale-125" href="https://github.com/santamaa" target="_blank" rel="noreferrer">
+                    <a className="w-10 md:w-12 h-10 md:h-12 grid place-items-center border-2 border-mywhite2 rounded-full bg-mywhite2 text-myblack3 text-xl md:text-2xl ease-in duration-300 hover:bg-myblack3 hover:text-mywhite2 hover:scale-125" href="https://github.com/santamaaa" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={ faGithub } />
                     </a>
                 </div>
@@ -88,9 +88,9 @@ function App() {
                     )
                 }
             </div>
-            <div className="mt-6 mb-3 py-4 md:mt-12 md:mb-6 flex items-center justify-end gap-1 text-mywhite2">
+            <div className="mt-20 mb-10 flex items-center justify-end gap-1 text-mywhite2">
                 <span className="text-md">&copy;</span>
-                <span className="text-xs">Copyright Santamaa</span>
+                <span className="text-xs">Copyright Santamaaa</span>
                 <CurrentYear />
             </div>
         </div>
--- src/assets/webdev-projects.js
+++ src/assets/webdev-projects.js
@@ -10,7 +10,7 @@ const webdevProjects = [
         "name": "Maknews",
         "desc": "Maknews is a project developed using ReactJS that aims to deliver the latest news quickly and accurately. By leveraging the services of currentsapi.services, Maknews can display up-to-date news from various reliable sources. Users can easily access the latest information presented in a responsive and intuitive interface, ensuring a pleasant and efficient news-reading experience.",
         "img": maknewsImg,
-        "url": "https://santamaa.github.io/maknews",
+        "url": "https://santamaaa.github.io/maknews",
         "tech": ["React", "Tailwind CSS", "Currents API"]
     },
     {
@@ -19,7 +19,7 @@ const webdevProjects = [
         "name": "Tokotok",
         "desc": "The TokoTok landing page project involves creating an engaging and modern entry point for the brand using ReactJS and TailwindCSS. The landing page will feature a captivating hero section, a product showcase with data from dummyjson.com. By integrating dynamic ReactJS components and leveraging TailwindCSS for a responsive design, the page will provide a visually appealing and functional experience that highlights TokoTok's key offerings and enhances user engagement.",
         "img": tokotokImg,
-        "url": "https://santamaa.github.io/tokotok",
+        "url": "https://santamaaa.github.io/tokotok",
         "tech": ["React", "Tailwind CSS", "DummyJSON"]
     },
     {
@@ -28,7 +28,7 @@ const webdevProjects = [
         "name": "Infriendtory API",
         "desc": "Infriendtory API is a comprehensive backend system designed to manage warehouse operations, built using Flask, MongoDB, and JWT Extended. The API provides powerful CRUD (Create, Read, Update, Delete) functionality to handle key data entities within the warehouse environment, including items, item categories, and suppliers. Utilizing MongoDB for flexible data storage and Flask to create scalable and efficient endpoints, this API ensures secure and smooth data operations. JWT Extended is used for authentication and authorization.",
         "img": infrientoryAPIImg,
-        "url": "https://santamaa-infriendtory-api.vercel.app",
+        "url": "https://santamaaa-infriendtory-api.vercel.app",
         "tech": ["Flask", "MongoDB", "JWT Extended"]
     },
     {
@@ -37,7 +37,7 @@ const webdevProjects = [
         "name": "Infriendtory Dashboard",
         "desc": "Infriendtory Dashboard is a warehouse application developed using ReactJS and TailwindCSS. This application is designed to streamline inventory management for retail stores by offering CRUD (Create, Read, Update, Delete) features. The Infriendtory Dashboard application integrates with the Infriendtory API to fetch and manage inventory data in real-time, allowing users to easily add, update, and delete product information. With a responsive and user-friendly interface, this application helps retail stores maintain their stock efficiently.",
         "img": infrientoryDashboardImg,
-        "url": "https://santamaa.github.io/infriendtory-dashboard/",
+        "url": "https://santamaaa.github.io/infriendtory-dashboard/",
         "tech": ["React", "TailwindCSS"]
     }
 ]
